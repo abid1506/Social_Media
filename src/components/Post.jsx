fb5a52889eaec0c6eff5c5fa84d792faafc1029a
Abid <mohammadabidali098@gmail.com>
2024-03-28 03:19:21 +0530

    Changes

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import {PostList} from "../store/post-list-store";
+import { FaDeleteLeft } from "react-icons/fa6";
+
+
+
+  const Post = ({post}) => {
+    const { deletePost } = useContext(PostList);
+  
+
+  return(
+
+  <div className="card post-card" style={{width: "30rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">{post.title} <span className="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle" onClick={() => deletePost(post.id)}>
+    <FaDeleteLeft />
+  </span></h5>
+    <p className="card-text">{post.body}</p>
+    {post.tags.map((tag) =>(
+    <span key={tag} className="badge text-bg-primary hashtag">{tag}</span>
+   ))}
+
+<div className="alert alert-success reactions" role="alert">
+ This post has been reacted by {post.reactions} people.
+</div>
+  </div>
+</div>
+  );
+    };
+export default Post;
